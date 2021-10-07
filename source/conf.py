0a95f606b09309af6f5355c5f56f5949c045b4d1
Jeff Hochberg <jeff@w4jew.com>
2021-10-07 03:46:00 -0400

    Reset Sphinx

--- source/conf.py
+++ source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'DigiPi Wiki'
-copyright = '2021, Jeff Hochberg'
-author = 'Jeff Hochberg'
+copyright = '2021, Jeff Hochberg - W4JEW'
+author = 'Jeff Hochberg - W4JEW'
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,7 +27,8 @@ author = 'Jeff Hochberg'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -43,8 +44,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = 'sphinx-rtd-theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- source/index.rst
+++ source/index.rst
@@ -1,5 +1,5 @@
 .. DigiPi Wiki documentation master file, created by
-   sphinx-quickstart on Thu Oct  7 03:30:26 2021.
+   sphinx-quickstart on Thu Oct  7 03:43:32 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -10,8 +10,8 @@ Welcome to DigiPi Wiki's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-   Welcome
-   Overview
+   welcome.md
+   overview.md
 
 Indices and tables
 ==================
